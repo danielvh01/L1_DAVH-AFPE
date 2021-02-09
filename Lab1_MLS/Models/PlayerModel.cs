abd3085ab2e56ce4cee056a3f0b9bc1073e92c41
danielvh01 <danielvelasquez32@gmail.com>
2021-02-09 15:34:01 -0600

    Creacion

---
+++ Lab1_MLS/Models/PlayerModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab1_MLS.Models
+{
+    public class PlayerModel : Controller
+    {        
+
+        public string Name { get; set; }
+
+        public string LastName { get; set; }
+        
+        public string Position { get; set; }
+
+        public double Salary { get; set; }
+
+        public string Club { get; set; }
+    }
+}
