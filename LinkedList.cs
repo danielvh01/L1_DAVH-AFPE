98ec99986c327eb9b58470a1469e497b25cbff1d
ANDRES FERNANDO PARADA ESTRADA <afparada@correo.url.edu.gt>
2021-02-15 15:59:38 -0600

    LinkedList and Node class

---
+++ LinkedList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace L1_DAVH_AFPE
+{
+
+    public class LinkedList : IEnumerable<out T>
+    {
+        public IEnumerator<T> GetEnumerator()
+        {
+            var node = First;
+            while(node != null)
+            {
+                yield return node.Value;
+                node = node.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
