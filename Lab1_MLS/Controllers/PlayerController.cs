ace76d219874b85ff224ef5e787fbde77b79e7fe
danielvh01 <danielvelasquez32@gmail.com>
2021-02-16 22:47:13 -0600

    Additional changes

--- Lab1_MLS/Controllers/PlayerController.cs
+++ Lab1_MLS/Controllers/PlayerController.cs
@@ -9,12 +9,15 @@ using System.IO;
 using Lab1_MLS.Models;
 using System.Web;
 using Microsoft.AspNetCore.Hosting;
+using System.Diagnostics;
 
 namespace Lab1_MLS.Controllers
 {
     public class PlayerController : Controller
     {
+        
         int cont = 0;
+        Stopwatch conteo = new Stopwatch();
         private readonly IHostingEnvironment hostingEnvironment;
         public PlayerController(IHostingEnvironment hostingEnvironment)
         {
@@ -24,6 +27,7 @@ namespace Lab1_MLS.Controllers
         public ActionResult Index()
         {
             return View(Singleton.Instance.PlayersList);
+            //return View(Handcraftted list);
         }
 
 
@@ -136,6 +140,7 @@ namespace Lab1_MLS.Controllers
         {
             if (ModelState.IsValid)
             {
+                conteo.Start();
                 string uniqueFileName = null;
                 string filePath = null;
                 if (model.File != null)
@@ -189,6 +194,80 @@ namespace Lab1_MLS.Controllers
                 }
 
             }
+            conteo.Stop();
+            string timeElapsed = Convert.ToString(conteo.Elapsed);
+            //Necessary return time data to index
+            return RedirectToAction(nameof(Index));
+
+        }
+
+        public ActionResult Import_File()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Import_File(FileModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                conteo.Start();
+                string uniqueFileName = null;
+                string filePath = null;
+                if (model.File != null)
+                {
+                    string uploadsFolder = Path.Combine(hostingEnvironment.WebRootPath, "Uploads");
+                    uniqueFileName = Guid.NewGuid().ToString() + "_" + model.File.FileName;
+                    filePath = Path.Combine(uploadsFolder, uniqueFileName);
+                    model.File.CopyTo(new FileStream(filePath, FileMode.Create));
+                }
+                var lectorlinea = new StreamReader(model.File.OpenReadStream());
+                string linea = lectorlinea.ReadToEnd();
+                string[] players = linea.Split("\n");
+                for (int i = 1; i < players.Length; i++)
+                {
+                    string[] newPlayer = players[i].Split(',');
+                    if (newPlayer.Length == 6)
+                    {
+                        var PlayerAded = new Models.PlayerModel
+                        {
+                            Id = cont,
+                            Club = newPlayer[0],
+                            LastName = newPlayer[1],
+                            Name = newPlayer[2],
+                            Position = newPlayer[3],
+                            Salary = Double.Parse(newPlayer[4]),
+                            Compensation = Double.Parse(newPlayer[5])
+
+                        };
+                       //Agregar a double linked list
+                    }
+                    else
+                    {
+                        newPlayer = players[i].Split(';');
+                        if (newPlayer.Length == 6)
+                        {
+                            var PlayerAded = new Models.PlayerModel
+                            {
+                                Id = cont,
+                                Club = newPlayer[0],
+                                LastName = newPlayer[1],
+                                Name = newPlayer[2],
+                                Position = newPlayer[3],
+                                Salary = Double.Parse(newPlayer[4]),
+                                Compensation = Double.Parse(newPlayer[5])
+
+                            };
+                            //Agregar a double linked list
+                        }
+                    }
+                    cont++;
+                }
+
+            }
+            conteo.Stop();
+            string timeElapsed = Convert.ToString(conteo.Elapsed);
+            //Necessary return time data to index
             return RedirectToAction(nameof(Index));
 
         }
