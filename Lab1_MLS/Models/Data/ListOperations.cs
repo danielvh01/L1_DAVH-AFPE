ad19d51f3a866dc1f314393ba4ea0e6de89d0bfd
ANDRES FERNANDO PARADA ESTRADA <afparada@correo.url.edu.gt>
2021-02-19 07:55:01 -0600

    Search

---
+++ Lab1_MLS/Models/Data/ListOperations.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab1_MLS.Models.Data
+{
+    public class ListOperations
+    {
+        public IEnumerable<object> Search(IEnumerable<object> list, Predicate<object> Comparer)
+        {
+            List<object> result = new List<object>();
+            for(int i  = 0; i < list.Count(); i++)
+            {
+                if(Comparer.Invoke(list.ElementAt(i)))
+                {
+                    result.Add(list.ElementAt(i));
+                }
+            }
+            return result;
+        }
+    }
+}
